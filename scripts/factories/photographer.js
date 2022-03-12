76d318f6ff53a718b8e8e09617c92db603d0e2c5
Cloé Boudrant <cloeboudrant@protonmail.com>
2022-03-12 10:21:40 +0100

    mise en place des portraits photographers

--- scripts/factories/photographer.js
+++ scripts/factories/photographer.js
@@ -1,17 +1,35 @@
 function photographerFactory(data) {
-    const { name, portrait } = data;
 
+  function getUserCardDOM() {
+    const { portrait, name, city, country, tagline, price, id } = data;
+    
     const picture = `assets/photographers/${portrait}`;
+    const article = document.createElement('article');
+    const img = document.createElement('img');
+    const h2 = document.createElement('h2');
+    const h3 = document.createElement('h3');
+    const h4 = document.createElement('h4');
+    const p = document.createElement('p');
 
-    function getUserCardDOM() {
-        const article = document.createElement( 'article' );
-        const img = document.createElement( 'img' );
-        img.setAttribute("src", picture)
-        const h2 = document.createElement( 'h2' );
-        h2.textContent = name;
-        article.appendChild(img);
-        article.appendChild(h2);
-        return (article);
-    }
-    return { name, picture, getUserCardDOM }
+    article.addEventListener('click', () => {
+      window.location.href = `photographer.html ? id=${id}`;
+    })
+
+    img.setAttribute('src', picture);
+    img.setAttribute('alt', 'photo' + '' + name);
+    h2.textContent = name;
+    h2.setAttribute('aria-label', name);
+    h3.textContent = city + ', ' + country;
+    h4.textContent = tagline;
+    p.textContent = price + '€/jour';
+
+
+    article.appendChild(img);
+    article.appendChild(h2);
+    article.appendChild(h3);
+    article.appendChild(h4);
+    article.appendChild(p);
+    return (article);
+  }
+  return {getUserCardDOM};
 }
